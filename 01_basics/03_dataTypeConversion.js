96d3e2f2b6e06ed2568daf065b4abf63a4e4187e
Anurag Verma <anuragverma4814@gmail.com>
2026-09-15 16:53:14 +0000

    Added 3rd files

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,57 @@
+// let score="Anurag"
+// console.log(score)
+// console.log(typeof score) //or 
+// console.log(typeof(score)) // both can be written
+// gives string as o/p
+// let valueInNumber = Number(score) 
+// for converting in number
+// console.log(typeof(valueInNumber)); //=> gives type number
+//"33" => 33
+//"333abc" => if want to convert in number it gives NaN(not a number)
+// true =>1; false => 0 in bool conversion
+
+// let isLoggedIn = "Anurag"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// converting the string in boolean
+// console.log(booleanIsLoggedIn);
+// 1=>true; 0=>false
+//""=> false (empty sting conversion in bool)
+// "Anurag" => true;
+// let someNumber=33 //type is number
+// let stringNumber=string(someNumber);// converted number in string
+// console.log(stringNumber);
+ // output will be 33 but type will be string
+
+ // *************** OPERATIONS *****************
+
+// let value=3
+// let negvalue=-value;
+ // console.log(negValue); gives -3
+ // basic operations
+//  console.log(4+2)
+//  console.log(4-2)
+//  console.log(4*2)
+//  console.log(4/2)
+//  console.log(4%2) // remainder
+//  console.log(4**2) => gives power of 4 the power 2
+ 
+// let str1 = "Hello"
+// let str2 = "World"
+// let str3 = str1 + str2 //give added string that is HelloWorld
+
+console.log("1"+2) // give 12
+console.log(1+"2") // give 12
+console.log("1"+2+2) // give 122
+console.log(1+2+"2")//==(1+2)+"2" that gives=32
+console.log(3+4*5%2) // don't prefer these type of code use paranthesis
+// value will be calculated by operator precedence
+console.log(true)//gives ture
+console.log(+true) // gives 1
+//console.log(true+)//not possible error
+console.log(+"")// as "" gives 0 in bolean=> output=0
+let x,y,z;
+x = y = z = 4
+// don't define this type
+ let count=10;
+ count++; // use then increase
+ ++count; // increase then use
